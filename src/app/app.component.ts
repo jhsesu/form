83deb5d8792933de520363117468829d75b41468
Jose Sernaqué <Jose Sernaqu>
2016-12-05 17:16:06 +0000

    Commit terminal 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app', //'app-root',
+  template: `<hero-form></hero-form>`//'./app.component.html',
+  /*styleUrls: ['./app.component.css']*/
+})
+export class AppComponent {
+  //title = 'app works!';
+}
